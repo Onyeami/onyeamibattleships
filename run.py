fa451c29f732647809f36cabe54a00f79de03238
Onyeami <>
2023-10-13 20:06:03 +0000

    creating the menu options

--- run.py
+++ run.py
@@ -101,10 +101,9 @@ def play_game(size, num_ships, level, time_limit):
             # Player's turn
             print("Player's turn:")
             start_time = time.time()
-            guess = input(
-                "Enter your guess(e.g., A1)
-                or type 'status' to check the current status: "
-            ).upper()
+            guess = input
+            ("Enter your guess(e.g., A1)
+             or type 'status' to check the current status: ").upper()
 
             if guess == 'STATUS':
                 display_game_status(
@@ -262,5 +261,76 @@ def main():
                 "Select difficulty level: ", type_=int, min_=1, max_=3
             )
         elif choice == 3:
-            print("Thank you for playing Battleships!")
+            print("Thank you for playing Onyeami's Battleships game!")
             break
+
+
+# Function to create the main menu
+def main_menu():
+    print("1. Start Game")
+    print("2. Difficulty Level")
+    print("3. Quit")
+
+
+# Function to create the difficult level
+def difficulty_menu():
+    print("1. Easy (30 seconds per turn)")
+    print("2. Medium (20 seconds per turn)")
+    print("3. Hard (10 seconds per turn)")
+
+
+def get_user_input(prompt, type_=None, min_=None, max_=None, range_=None):
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if min_ is not None and ui < min_:
+            print("Input must be at least {0}.".format(min_))
+        elif max_ is not None and ui > max_:
+            print("Input must be at most {0}.".format(max_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    print(template.format(" or ".join(map(str, range_))))
+        else:
+            return ui
+
+
+def main():
+    size = 5  # Default grid size
+    num_ships = 3  # Default number of ships
+    level = 1  # Default difficulty level (1: Easy, 2: Medium, 3: Hard)
+    time_limits = {1: 30, 2: 20, 3: 10}  # Time limit for each difficulty level
+
+    while True:
+        main_menu()
+        choice = get_user_input(
+            "Enter your choice: ", type_=int, min_=1, max_=3
+        )
+
+        if choice == 1:
+            play_game(size, num_ships, level, time_limits[level])
+        elif choice == 2:
+            difficulty_menu()
+            level = get_user_input(
+                "Select difficulty level: ", type_=int, min_=1, max_=3
+            )
+        elif choice == 3:
+            print("Thank you for playing Onyeami's Battleships game!")
+            break
+
+
+if __name__ == "__main__":
+    main()
