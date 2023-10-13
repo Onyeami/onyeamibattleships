7d35d1a3b5f9c8008e1819e23120722dadfc44b7
Onyeami <>
2023-10-13 08:15:07 +0000

    created a function for the grid format

--- run.py
+++ run.py
@@ -6,3 +6,14 @@ import time
 # Function to create a grid of given size filled with 'o'
 def create_grid(size):
     return [['o'] * size for _ in range(size)]
+
+
+# Function to print the grid in a nice format
+def print_grid(grid):
+    size = len(grid)
+    # Print column headers (A, B, C, ...)
+    print("   " + " ".join([chr(65+i) for i in range(size)]))
+    for i in range(size):
+        # Print each row with row number
+        print(str(i+1).rjust(2) + "|" + " ".join(grid[i]) + "|")
+        print("-" * (2*size+5))  # Print line separator
