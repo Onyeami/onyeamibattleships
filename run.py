19a4fbdb90384715e74087e1520df72eec24c79a
Onyeami <>
2023-10-13 08:07:54 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import the random module to generate random numbers
+import random
+import time
+
+
+# Function to create a grid of given size filled with 'o'
+def create_grid(size):
+    return [['o'] * size for _ in range(size)]
